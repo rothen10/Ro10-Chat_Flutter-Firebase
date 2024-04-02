054bfccf51eb23951ee7239803ea2b0645bb419c
rothen10 <rothensohmai@gmail.com>
2024-04-02 17:34:37 +0200

    chat-finished

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = '1:483622794125:android:9f9e7f1bb2df6d2763c3c4';
+  static String apiKey = "AIzaSyAvspTzFpd4fWumTrS4FwqM-E64sjY368w";
+  static String messagingSenderId = "483622794125";
+  static String projectId = "chatappflutter-987d0";
+  final primaryColor = const Color(0xFFee7b64);
+}
